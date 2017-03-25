325e95edf94893ec013239c4e4db3b12d2660fc3
jshobhitj <jshobhitj.7@gmail.com>
2017-03-25 16:49:43 -0500

    Script for cleaning & tagging wikipedia data.

---
+++ README.md
@@ -0,0 +1,12 @@
+# nlp-fact-extractor
+The web is full of informative free-text resources, which might make some sense to a human being but not much to a computer, except might be as unstructured data or a bag of words. In this project, we are trying to implement Snowball system for the crucial task of extracting information/facts from massive free-text resources, such as Wikipedia and generating a database (semantic) of facts and information.
+
+Semantic data allow machines to interact with worldly information without human interpretation. Semantic representation of data involves two entities and the relationship between them. For example, the sentence “Einstein was born in Germany.” would be represented as <Einstein, Germany> and the relationship would be "wasBornIn".
+
+References:
+
+[1] E. Agichtein and L. Gravano. Snowball: extracting relations from large plain-text collections. In ICDL, 2000.
+
+[2] Information extraction from Wikipedia using pattern learning, Márton Miháltz & Péter Pázmány, Journal Acta Cybernetica archive Volume 19 Issue 4, January 2010 Issue-in-Progress, Pages 677-694.
+
+[3] YAGO: A Core of Semantic Knowledge Unifying WordNet and Wikipedia, Fabian M. Suchanek, Gjergji Kasneci & Gerhard Weikum, Proceeding WWW '07 Proceedings of the 16th international conference on World Wide Web, Pages 697-706.
---
+++ Snowball/Get_Cleaned_Tagged_Data.py
@@ -0,0 +1,155 @@
+import os
+import bz2
+import urllib
+import time
+
+from nltk.tag import StanfordNERTagger
+
+
+def download_data(link, loc, delete_tmp=True):
+    st = time.time()
+    print "\nDownloading data from (this may take time): " + link
+    if not os.path.exists(loc):
+        os.makedirs(loc)
+
+    file_path = loc + 'articles.xml.bz2'
+    urllib.urlretrieve(link, file_path)
+    print "Download Complete."
+    print "Time Taken: %s ms" % str(time.time() - st)
+
+    if not os.path.exists(file_path):
+        return
+
+    st = time.time()
+    print '\nExtracting bz2 compressed file...'
+    zipfile = bz2.BZ2File(file_path)  # open the file
+    data = zipfile.read()  # get the decompressed data
+    new_file_path = file_path[:-4]  # assuming the file_path ends with .bz2
+    open(new_file_path, 'wb').write(data)  # write a uncompressed file
+    zipfile.close()
+    print "Time Taken: %s ms" % str(time.time() - st)
+
+    if delete_tmp:
+        os.remove(file_path)
+
+
+def extract_data(loc, delete_tmp=True):
+    st = time.time()
+    print "\nExtracting necessary articles from wiki data..."
+
+    f = open(loc + 'articles.xml', 'r')
+    extracted_articles = bz2.BZ2File(loc + 'extracted_articles.xml.bz2', 'w')
+    # TODO: Creating a fact file from infoboxes for testing
+    fact_file = open('./data/facts', 'w')
+
+    # Copying xml schema definitions to extracted file
+    for line in f:
+        if "<page>" not in line:
+            extracted_articles.write(line)
+        else:
+            break
+
+    # Copying only relevant articles from wiki data
+    # to extracted data file (i.e. only company wiki pages)
+    idx = 1
+    f1 = open(loc + str(idx) + '.xml', 'w')
+    flag = False
+
+    for line in f:
+        if "<page>" in line:
+            f1 = open(loc + str(idx) + '.xml', 'w')
+            f1.write(line)
+            flag = False
+        elif "Infobox company" in line:
+            flag = True
+            f1.write(line)
+        elif "</page>" in line:
+            if flag:
+                f1.write(line)
+                f1.close()
+
+                f1 = open(loc + str(idx) + '.xml', 'r')
+                extracted_articles.write(f1.read())
+                f1.close()
+                os.remove(loc + str(idx) + '.xml')
+
+                if idx % 100 == 0:
+                    print 'Extracted articles count: ' + str(idx)
+                idx += 1
+        else:
+            f1.write(line)
+
+    try:
+        f1.close()
+        if not flag:
+            os.remove(loc + str(idx) + '.xml')
+            idx -= 1
+    except:
+        pass
+
+    extracted_articles.write('</mediawiki>')
+    extracted_articles.close()
+    f.close()
+    print 'Extraction Complete.'
+    print 'Total extracted articles: ' + str(idx - 1)
+    print 'Time taken: %s ms' % str(time.time() - st)
+
+    if delete_tmp:
+        os.remove(loc + 'articles.xml')
+
+
+def clean_data(loc, delete_tmp=True):
+    st = time.time()
+    print '\nCleaning articles to extract text only...'
+    os.system('python ./lib/WikiExtractor.py -b 1000K -o ' + loc + ' ' + loc + 'extracted_articles.xml.bz2')
+    print 'Time taken: %s ms' % str(time.time() - st)
+
+    if delete_tmp:
+        os.remove(loc + 'extracted_articles.xml.bz2')
+
+
+def get_data(link, loc, delete_tmp=True):
+    if not os.path.exists(loc):
+        os.makedirs(loc)
+    download_data(link, loc, delete_tmp)
+    extract_data(loc, delete_tmp)
+    clean_data(loc, delete_tmp)
+
+
+def get_cleaned_and_tagged_data(link, delete_tmp=True):
+    tmp_loc = './tmp/'
+    get_data(link, tmp_loc)
+
+    data_loc = './data/'
+    if not os.path.exists(data_loc):
+        os.makedirs(data_loc)
+
+    st = time.time()
+    idx = 0
+    print "\nTagging data using Stanford NER Tagger (this may take time)..."
+
+    st = StanfordNERTagger('./lib/english.all.3class.distsim.crf.ser.gz', './lib/stanford-ner.jar')
+    for subdir, dirs, files in os.walk(tmp_loc):
+        for file in files:
+            r_file_path = os.path.join(subdir, file)
+            f = open(r_file_path, 'r')
+
+            w_file_path = data_loc + r_file_path[len(tmp_loc):]
+
+            if not os.path.exists(os.path.dirname(w_file_path)):
+                os.makedirs(os.path.dirname(w_file_path))
+
+            td = open(w_file_path, 'w')
+            for line in f:
+                td.write(str(st.tag(line.split())))
+            td.close()
+            idx += 1
+
+            if idx % 5 == 0:
+                print 'Tagging done for %s files' % str(idx)
+
+    print 'Tagging Complete.'
+    print 'Time taken: %s ms' % str(time.time() - st)
+
+
+get_cleaned_and_tagged_data("https://dumps.wikimedia.org/enwiki/20170320/enwiki-20170320-pages-articles1.xml-p000000010p000030302.bz2")
