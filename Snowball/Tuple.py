566d0d8f20e53904118c5751fa1249106fb10bcf
bhavik ameta <bhavik ameta>
2017-03-25 23:09:05 -0500

    seed tuple parsing

--- Snowball/Tuple.py
+++ Snowball/Tuple.py
@@ -1,8 +1,8 @@
 
 class Tuple():
-    def __init__(self, config):
-        self.tag1_value = ""
-        self.tag2_value = ""
+    def __init__(self, config, tag1_value, tag2_value):
+        self.tag1_value = tag1_value
+        self.tag2_value = tag2_value
         self.tag1 = config.tag1
         self.tag2 = config.tag2
         
@@ -13,5 +13,7 @@ class Tuple():
         self.patterns = []
         self.confidence = 0.0
         
-        self.is_pos = True 
+        self.is_pos = True
         
+    def __repr__(self):
+        return self.tag1 + " : " + self.tag1_value + ", " + self.tag2 + " : " + self.tag2_value
